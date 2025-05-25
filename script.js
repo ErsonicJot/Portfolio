ea794e13f1989563bd576bf109a2b5aa12778026
ErsonicJot <jasveersohian@gmail.com>
2025-05-25 18:09:51 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('x');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('item1');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('item2');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('item3');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('item4');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
+window.addEventListener('scroll', function () {
+    const header = document.getElementById('item5');
+    if (window.scrollY > 0) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
